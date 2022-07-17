5636a5097c7a96915606e27258bc9a6fae099b95
eric <elavagnim@gmail.com>
2022-07-17 14:45:26 -0500

    Partially completed Banking Microserice

--- MicroRabbit.Banking.Api/Program.cs
+++ MicroRabbit.Banking.Api/Program.cs
@@ -1,6 +1,49 @@
+
+using MediatR;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<BankingDbContext>(item => item.UseSqlServer(builder.Configuration.GetConnectionString("BankingDbConnection")));
+builder.Services.AddControllers();
+
+builder.Services.AddMediatR(typeof(Program));
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Baking Microservice", Version = "v1" });
+});
+
+
+RegisterServices(builder.Services);
+
+builder.Services.AddDbContext<BankingDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BankingDbConnection"));
+});
+
 var app = builder.Build();
+
+void RegisterServices(IServiceCollection services)
+{
+    DependencyContainer.RegisterServices(services);
+}
 
 app.MapGet("/", () => "Hello World!");
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice V1");
+});
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 app.Run();
+
+
+
