3d15408b54643b08d714ed9a4beecd2e99273cf2
eric <elavagnim@gmail.com>
2022-07-30 14:23:37 -0500

    Create MVC project to test banking microservice

---
+++ MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient) 
+        { 
+            _apiClient = apiClient;
+        }
+
+
+        public async Task<HttpResponseMessage> Transfer(TransferDto transferDto)
+        {
+            var uri = "http://localhost:5151/api/banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto), System.Text.Encoding.UTF8, "application/json");
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            return response.EnsureSuccessStatusCode();
+        }
+    }
+}
